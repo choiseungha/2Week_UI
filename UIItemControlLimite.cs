d75f3102fa928ec1421c2a0c0ccfaa66b9ec3c1f
SeungHaChoi <52003145+SeungHaChoi@users.noreply.github.com>
2020-04-20 02:37:38 +0900

    Image_String_Update

---
+++ UIItemControlLimite.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Du3Project
+{
+    [RequireComponent(typeof(InfiniteScroll))]
+    public class UIItemControlLimite : MonoBehaviour, IInfiniteScrollSetup
+    {
+        public Sprite[] ItemImage_List ;
+        public string[] labels;
+        public int ItemCount = 20;
+
+        public void OnPostSetupItems()
+        {
+            InfiniteScroll scroll = GetComponent<InfiniteScroll>();
+            scroll.onUpdateItem.AddListener(OnUpdateItem);
+            GetComponentInParent<ScrollRect>().movementType = ScrollRect.MovementType.Elastic;
+
+
+            // content size 수정하기
+            var rectTransform = GetComponent<RectTransform>();
+            var delta = rectTransform.sizeDelta;
+            if(scroll.DirectionVal == InfiniteScroll.Direction.Vertical)
+            {
+                delta.y = scroll.itemScale * ItemCount;
+                rectTransform.sizeDelta = delta;
+            }
+            else
+            {
+                delta.x = scroll.itemScale * ItemCount;
+                rectTransform.sizeDelta = delta;
+            }
+            
+        }
+
+        public void OnUpdateItem(int itemCount, GameObject obj)
+        {
+            UIItemElement uiitem = obj.GetComponent<UIItemElement>();
+
+            // 사이즈 지정
+            if(itemCount >= 0
+                && itemCount < ItemCount )
+            {
+                uiitem.gameObject.SetActive(true);
+                uiitem.UpdateItem(itemCount, ItemImage_List[itemCount], labels[itemCount]);
+            }
+            else
+            {
+                uiitem.gameObject.SetActive(false);
+            }
+
+            
+        }
+
+        public void DirectUpdate(GameObject obj)
+        {
+        }
+
+        void Start()
+		{
+			
+		}
+
+		//void Update()
+		//{
+			
+		//}
+	}
+}
---
+++ UIItemElement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Du3Project
+{
+	public class UIItemElement : MonoBehaviour
+	{
+        [Header("[자료들]")]
+        public Text ItemLabel = null;
+        public Text ItemScore = null;
+        public Image ItemIcon = null;
+
+        [Header("[데이터용Index]")]
+        public int ItemIndex = -1;
+
+
+        public void UpdateItem(int p_index, Sprite p_item = null, string p_label = "happy")
+        {
+            ItemIndex = p_index;
+
+            ItemScore.text = ItemIndex.ToString();
+            ItemIcon.sprite = p_item;
+            ItemLabel.text = p_label;
+        }
+
+        void Start()
+		{
+			
+		}
+
+		//void Update()
+		//{
+			
+		//}
+	}
+}
